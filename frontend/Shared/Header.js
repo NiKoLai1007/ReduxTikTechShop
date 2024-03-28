40e8cb19b60da5274572d52bab8eb5c2422a58e2
Jurome <jaspherodevilas.09@gmail.com>
2024-03-28 19:43:50 +0800

    added Shared folder and added carticon, error, and header.js, and added on assests folder

---
+++ frontend/Shared/Header.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { StyleSheet, Image, SafeAreaView, View } from "react-native"
+
+const Header = () => {
+    return (
+        //<View style={styles.header}>
+        <SafeAreaView style={styles.header}>
+            <Image
+                source={require("../assets/logo.png")}
+                resizeMode="contain"
+                style={{ height: 50 }}
+            />
+
+        </SafeAreaView>
+        //</View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: "100%",
+        flexDirection: 'row',
+        alignContent: "center",
+        justifyContent: "center",
+        padding: 20,
+        marginTop: 80,
+    }
+})
+
+export default Header
