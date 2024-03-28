3e9680cd2d68b841184f05ed28bbfc3aef3f06ad
ecapaglingayen@gmail.com <eumancarl.paglingayen@tup.edu.ph>
2024-03-28 18:27:29 +0800

    Adding in Admin/Cart/Product/User

---
+++ frontend/Screens/Product/SingleProduct.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import { Image, View, StyleSheet, Text, ScrollView, Button } from "react-native";
+import { Left, Right, Container, H1, Center, Heading } from 'native-base'
+import EasyButton from "../../Shared/StyledComponents/EasyButton"
+import TrafficLight from '../../Shared/StyledComponents/TrafficLight'
+const SingleProduct = ({ route }) => {
+    const [item, setItem] = useState(route.params.item);
+    // console.log(item)
+    const [availability, setAvailability] = useState('')
+    const [availabilityText, setAvailabilityText] = useState("")
+    useEffect(() => {
+        if (item.countInStock == 0) {
+            setAvailability(<TrafficLight unavailable></TrafficLight>);
+            setAvailabilityText("Unvailable")
+        } else if (item.countInStock <= 5) {
+            setAvailability(<TrafficLight limited></TrafficLight>);
+            setAvailabilityText("Limited Stock")
+        } else {
+            setAvailability(<TrafficLight available></TrafficLight>);
+            setAvailabilityText("Available")
+        }
+
+        return () => {
+            setAvailability(null);
+            setAvailabilityText("");
+        }
+    }, [])
+    return (
+        <Center flexGrow={1}>
+            <ScrollView style={{ marginBottom: 80, padding: 5 }}>
+                <View>
+                    
+                    <Image
+                        source={{
+                            uri: item.image ? item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                        }}
+                        resizeMode="contain"
+                        style={styles.image}
+                    />
+
+                </View> 
+                <View style={styles.contentContainer}>
+                    <Heading style={styles.contentHeader} size='xl'>{item.name}</Heading>
+                    <Text style={styles.contentText}>{item.brand}</Text>
+                </View>
+                <View style={styles.availabilityContainer}>
+                    <View style={styles.availability}>
+                        <Text style={{ marginRight: 10 }}>
+                            Availability: {availabilityText}
+                        </Text>
+                        {availability}
+                    </View>
+                    <Text>{item.description}</Text>
+                </View>
+                <EasyButton
+                    primary
+                    medium
+                >
+
+                    <Text style={{ color: "white" }}> Add</Text>
+                </EasyButton>
+            </ScrollView>
+
+        </Center >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        height: '100%',
+
+    },
+    imageContainer: {
+        backgroundColor: 'white',
+        padding: 0,
+        margin: 0
+    },
+    image: {
+        width: '100%',
+        height: undefined,
+        aspectRatio: 1
+    },
+    contentContainer: {
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    contentHeader: {
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    contentText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    bottomContainer: {
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        backgroundColor: 'white'
+    },
+    price: {
+        fontSize: 24,
+        margin: 20,
+        color: 'red'
+    },
+    availabilityContainer: {
+        marginBottom: 20,
+        alignItems: "center"
+    },
+    availability: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    }
+})
+
+export default SingleProduct
