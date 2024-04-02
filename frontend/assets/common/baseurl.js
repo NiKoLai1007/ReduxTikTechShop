58c444974cdebf2015d9f43b016e0b0b2eabaad0
NiKoLai1007 <shanpalima10@gmail.com>
2024-04-03 00:28:42 +0800

    error

--- frontend/assets/common/baseurl.js
+++ frontend/assets/common/baseurl.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.0.27:4000/api/v1/'
+? baseURL = 'http://192.168.27.88:4000/api/v1/'
 : baseURL = 'http://192.168.0.27:4000/api/v1/'
 }
 
